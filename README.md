f5b50360df126927071d7531ab048c3cb4f0e392
Lance Finney <LMFinney@users.noreply.github.com>
2017-09-01 13:29:27 -0500

    Updated the test and readme to incorporate matches() for reducers.

--- README.md
+++ README.md
@@ -23,9 +23,110 @@ Install:
 npm install ngrx-enums
 ```
 
+## Example
+These examples are included in a [test](test/reducer.test.ts).
+
+### Actions Defined via Enums
+```typescript
+import {ActionEnum, ActionEnumValue} from '../src/action-enum';
+
+class LayoutAction<T> extends ActionEnumValue<T> {
+  constructor(name: string) {
+    super(name);
+  }
+}
+
+class LayoutActionEnumType extends ActionEnum<LayoutAction<any>> {
+  OPEN_SIDENAV: LayoutAction<void> =
+    new LayoutAction<void>('[Layout] Open Sidenav');
+  OPEN_SIDENAV_ALSO: LayoutAction<void> =
+    new LayoutAction<void>('[Layout] Open Sidenav Also');
+  SET_SIDENAV: LayoutAction<boolean> =
+    new LayoutAction<boolean>('[Layout] Set Sidenav');
+
+  constructor() {
+    super();
+    this.initEnum('layoutActions');
+  }
+}
+
+const LayoutActionEnum: LayoutActionEnumType = new LayoutActionEnumType();
+```
+
+### Reducer Defined via `matches()` Methods
+```typescript
+import {TypedAction} from '../src/action-enum';
+import {simplePropertyReducer} from '../src/reducer-enum';
+
+interface State {
+  showSidenav: boolean;
+}
+
+const initialState: State = {
+  showSidenav: false
+};
+
+function layoutReducer(state = initialState, action: TypedAction<any>): State {
+  if (LayoutActionEnum.matches(action,
+      LayoutActionEnum.OPEN_SIDENAV, LayoutActionEnum.OPEN_SIDENAV_ALSO)) {
+    // a simple reducer that always sets the same value
+    // this shows how to respond to multiple actions
+    // (fall-through in switch)
+    return {...state, showSidenav: true};
+  } else if (LayoutActionEnum.SET_SIDENAV.matches(action)) {
+    // a reducer that accepts a value and copies it as a property into state
+    return simplePropertyReducer<State, boolean>('showSidenav')(state, action);
+  } else {
+    return state;
+  }
+}
+```
+
+### Reducer Defined via Enums
+```typescript
+import {ActionEnumValue} from '../src/action-enum';
+import {ReducerEnum, ReducerEnumValue, ReducerFunction, simplePropertyReducer} from '../src/reducer-enum';
+
+interface State {
+  showSidenav: boolean;
+}
+
+const initialState: State = {
+  showSidenav: false
+};
+
+class LayoutReducer<T> extends ReducerEnumValue<State, T> {
+  constructor(action: ActionEnumValue<T> | ActionEnumValue<T>[],
+              reduce: ReducerFunction<State, T>) {
+    super(action, reduce);
+  }
+}
+
+class LayoutReducerEnumType extends ReducerEnum<LayoutReducer<any>, State> {
+  // a simple reducer that always sets the same value
+  // this shows how to respond to multiple actions
+  // (fall-through in switch)
+  OPEN_SIDENAV: LayoutReducer<void> =
+    new LayoutReducer<void>([LayoutActionEnum.OPEN_SIDENAV,
+        LayoutActionEnum.OPEN_SIDENAV_ALSO],
+      (state: State) => ({showSidenav: true}));
+  // a reducer that accepts a value and copies it as a property into state
+  SET_SIDENAV: LayoutReducer<boolean> = new LayoutReducer<boolean>(
+    LayoutActionEnum.SET_SIDENAV,
+    simplePropertyReducer<State, boolean>('showSidenav')
+  );
+
+  constructor() {
+    super(initialState);
+    this.initEnum('layoutReducers');
+  }
+}
+
+const LayoutReducerEnum: LayoutReducerEnumType = new LayoutReducerEnumType();
+```
+
 ## More information
 
-* The [test directory](test) contains examples.
 * Some users get an error that looks something like this when compiling:
  
    ```ERROR in Error encountered resolving symbol values statically. Calling function 'ɵmakeDecorator', function calls are not supported. Consider replacing the function or lambda with a reference to an exported function, resolving symbol Injectable...```
---
+++ test/reducer.test.ts
@@ -0,0 +1,159 @@
+import {ActionEnum, ActionEnumValue, TypedAction} from '../src/action-enum';
+import {
+  ReducerEnum,
+  ReducerEnumValue,
+  ReducerFunction,
+  simplePropertyReducer
+} from '../src/reducer-enum';
+
+class LayoutAction<T> extends ActionEnumValue<T> {
+  constructor(name: string) {
+    super(name);
+  }
+}
+
+class LayoutActionEnumType extends ActionEnum<LayoutAction<any>> {
+  OPEN_SIDENAV: LayoutAction<void> = new LayoutAction<void>(
+    '[Layout] Open Sidenav'
+  );
+  OPEN_SIDENAV_ALSO: LayoutAction<void> = new LayoutAction<void>(
+    '[Layout] Open Sidenav Also'
+  );
+  SET_SIDENAV: LayoutAction<boolean> = new LayoutAction<boolean>(
+    '[Layout] Set Sidenav'
+  );
+
+  constructor() {
+    super();
+    this.initEnum('layoutActions');
+  }
+}
+
+const LayoutActionEnum: LayoutActionEnumType = new LayoutActionEnumType();
+
+interface State {
+  showSidenav: boolean;
+}
+
+const initialState: State = {
+  showSidenav: false
+};
+
+describe('enum version', () => {
+  class LayoutReducer<T> extends ReducerEnumValue<State, T> {
+    constructor(
+      action: ActionEnumValue<T> | ActionEnumValue<T>[],
+      reduce: ReducerFunction<State, T>
+    ) {
+      super(action, reduce);
+    }
+  }
+
+  class LayoutReducerEnumType extends ReducerEnum<LayoutReducer<any>, State> {
+    // a simple reducer that always sets the same value
+    // this shows how to respond to multiple actions
+    // (fall-through in switch)
+    OPEN_SIDENAV: LayoutReducer<void> = new LayoutReducer<void>(
+      [LayoutActionEnum.OPEN_SIDENAV, LayoutActionEnum.OPEN_SIDENAV_ALSO],
+      (state: State) => ({showSidenav: true})
+    );
+    // a reducer that accepts a value and copies it as a property into state
+    SET_SIDENAV: LayoutReducer<boolean> = new LayoutReducer<boolean>(
+      LayoutActionEnum.SET_SIDENAV,
+      simplePropertyReducer<State, boolean>('showSidenav')
+    );
+
+    constructor() {
+      super(initialState);
+      this.initEnum('layoutReducers');
+    }
+  }
+
+  const LayoutReducerEnum: LayoutReducerEnumType = new LayoutReducerEnumType();
+
+  describe('OPEN_SIDENAV', () => {
+    it('should set the value', () => {
+      let state: State = LayoutReducerEnum.OPEN_SIDENAV.reduce(
+        initialState,
+        LayoutActionEnum.OPEN_SIDENAV.toAction()
+      );
+
+      expect(state.showSidenav).toBe(true);
+    });
+  });
+
+  describe('SET_SIDENAV', () => {
+    it('should set the value', () => {
+      let state: State = LayoutReducerEnum.SET_SIDENAV.reduce(
+        initialState,
+        LayoutActionEnum.SET_SIDENAV.toAction(true)
+      );
+
+      expect(state.showSidenav).toBe(true);
+
+      state = LayoutReducerEnum.SET_SIDENAV.reduce(
+        state,
+        LayoutActionEnum.SET_SIDENAV.toAction(false)
+      );
+
+      expect(state.showSidenav).toBe(false);
+    });
+  });
+});
+
+describe('Matches version', () => {
+  function layoutReducer(
+    state = initialState,
+    action: TypedAction<any>
+  ): State {
+    if (
+      LayoutActionEnum.matches(
+        action,
+        LayoutActionEnum.OPEN_SIDENAV,
+        LayoutActionEnum.OPEN_SIDENAV_ALSO
+      )
+    ) {
+      // a simple reducer that always sets the same value
+      // this shows how to respond to multiple actions
+      // (fall-through in switch)
+      return {...state, showSidenav: true};
+    } else if (LayoutActionEnum.SET_SIDENAV.matches(action)) {
+      // a reducer that accepts a value and copies it as a property into state
+      return simplePropertyReducer<State, boolean>('showSidenav')(
+        state,
+        action
+      );
+    } else {
+      return state;
+    }
+  }
+
+  describe('OPEN_SIDENAV', () => {
+    it('should set the value', () => {
+      let state: State = layoutReducer(
+        initialState,
+        LayoutActionEnum.OPEN_SIDENAV.toAction()
+      );
+
+      expect(state.showSidenav).toBe(true);
+    });
+  });
+
+  describe('SET_SIDENAV', () => {
+    it('should set the value', () => {
+      let state: State = layoutReducer(
+        initialState,
+        LayoutActionEnum.SET_SIDENAV.toAction(true)
+      );
+
+      expect(state.showSidenav).toBe(true);
+
+      state = layoutReducer(
+        state,
+        LayoutActionEnum.SET_SIDENAV.toAction(false)
+      );
+
+      expect(state.showSidenav).toBe(false);
+    });
+  });
+});
